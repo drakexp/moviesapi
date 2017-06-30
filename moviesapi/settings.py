2fa08dd0962229ffd16f9310a56ecd57acf3d227
David <drakexp@gmail.com>
2017-06-29 20:34:49 -0400

    settings

--- moviesapi/settings.py
+++ moviesapi/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 import os
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -86,7 +85,7 @@ DATABASES = {
         'USER': 'utyzpqklqbyjan',
         'HOST': 'ec2-174-129-227-116.compute-1.amazonaws.com',
     }
-}
+}.update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
